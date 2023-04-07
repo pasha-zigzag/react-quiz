c5b9c9385a327e8fe5624e5e23bfea8e37c34083
Pavel Zagainov <p.zagaynov@vk.team>
2023-04-07 13:16:37 +0300

    добавил контекст

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,13 +1,22 @@
 import Question from './Question';
-import { useState } from 'react';
+import { useContext  } from 'react';
+import { QuizContext } from '../contexts/quiz';
 
 const Quiz = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
+  console.log("state", quizState)
+
   return (
     <div className="quiz">
       <div>
         <div className="score">Question 1/8</div>
-        <Question/>
-        <div className="next-button">Next question</div>
+        <Question questions={quizState.questions} />
+        <div
+          className="next-button"
+          onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+        >
+          Next question
+        </div>
       </div>
     </div>
   );
